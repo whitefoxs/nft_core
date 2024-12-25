b77192ca5afb46c557d64902da8a3137759907ad
whitefoxs <whitefoxs2431@gmail.com>
2024-12-25 22:50:43 +0700

    Data Push

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Feel free to change "localhost" to an IP or domain if needed
-DB_USER = os.getenv("DB_USER", "zhenfenghuang")
+DB_USER = os.getenv("DB_USER", "zhengfenghuang")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "Fung24031978")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "nft_core_db")
